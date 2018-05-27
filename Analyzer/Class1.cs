3d6c87fefb72a5725b9384f98246e72214855dba
animah88 <animahwolf@gmail.com>
2018-05-27 20:23:11 +0300

    finaly add analyzer

---
+++ Analyzer/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyzer
+{
+    public class Analyzer
+    {
+        private static int erposition = 0;
+
+        public static string expression = "";
+
+        public static bool ShowMessage = true;
+
+        public static bool CheckCurrency()
+        {
+            int leftBracketCount = 0;
+            int rightBracketCount = 0;
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    leftBracketCount++;
+                }
+                if (expression[i] == ')')
+                {
+                    rightBracketCount++;
+                }
+            }
+
+            return (leftBracketCount == rightBracketCount);
+        }
+
+
+
+    }
+}
